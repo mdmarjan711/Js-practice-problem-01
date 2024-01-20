3f041f8cab18a84f350a11d6f44d0ae8ebdd26c1
mdmarjan711 <mdmarjan711@gmail.com>
2024-01-21 02:22:35 +0600

    added header

---
+++ array_1st.js
@@ -0,0 +1,55 @@
+var fruits=['mango', 'banana', 'jackfruit', 'lemon', 'papaya', 'chile'];
+var position = fruits.indexOf('papaya');
+console.log(position);//4
+
+var fruits=['mango', 'banana', 'jackfruit', 'lemon', 'papaya', 'chile'];
+console.log(fruits.length);
+
+var fruits=['mango', 'banana', 'jackfruit', 'lemon', 'papaya', 'chile'];
+fruits[1]='Track';
+fruits[5]='Bus';
+console.log(fruits);
+
+var fruits=['mango', 'banana', 'jackfruit', 'lemon', 'papaya', 'chile'];
+console.log(fruits);
+
+let fruits =['bus', 'track', 'jam'];
+console.log(fruits);
+
+var fruits=['mango', 'banana', 'jackfruit', 'lemon', 'papaya', 'chile'];
+
+var sp = 0;
+while(sp<=5){
+    console.log(sp,'vp vai');
+    sp++;
+}
+
+var sp = 1;
+while(sp<=10){
+    console.log(sp);
+    sp++;
+}
+
+
+var number = 0;
+while(number <= 10){
+    console.log(number);
+    number+=2; //0 2 4 6 8 10
+}
+
+//while loop
+var number = 1;
+while(number <= 10){
+    console.log(number);
+    number+=2; //1 3 5 7 9
+}
+
+//for loop
+for( var i = 1; i <= 7; i++){
+    console.log(i,"HBO i");
+}
+
+//for loop reverse print
+for (i = 5; i>= 1; i--){
+    console.log(i);
+}
